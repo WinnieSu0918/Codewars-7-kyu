d476358c48f394a7d6603ae15b67b0f8d6a1fb8f
WinnieSu0918 <su830918ggg@yahoo.com.tw>
2022-11-07 16:37:10 +0800

    feat: 2022-11-07 Add Solutions
    Description:
      - Reverse words

---
+++ Reverse words.js
@@ -0,0 +1,15 @@
+/*
+  Title:
+    Reverse words
+    
+  Description:
+    Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+    
+  Examples:
+    "This is an example!" ==> "sihT si na !elpmaxe"
+    "double  spaces"      ==> "elbuod  secaps"
+    
+*/
+
+// Now Solution
+const reverseWords = (str) => str.split(' ').map((item) => item.split('').reverse().join('')).join(' ')
