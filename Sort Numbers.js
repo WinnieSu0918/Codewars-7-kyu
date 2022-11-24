994b408ee699f8cee92fe745b0745dcd14bde177
WinnieSu0918 <su830918ggg@yahoo.com.tw>
2022-11-24 15:51:15 +0800

    feat: 2022-11-24 Add Solutions
    Description:
      - Sort Numbers

---
+++ Sort Numbers.js
@@ -0,0 +1,15 @@
+/*
+  Title:
+    Sort Numbers
+    
+  Description:
+    Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+    
+  Examples:
+    solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+    solution(null); // should return []
+    
+*/
+
+// Now Solution
+const solution = (nums) => nums ? nums.sort((a, b) => a - b) : []
